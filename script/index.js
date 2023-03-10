0302a39df7d808495c50c28b4f2e0e8a3c9794b6
YevaVlasova <eva-krechetova@ukr.net>
2023-03-10 19:01:32 +0200

    still working

--- script/index.js
+++ script/index.js
@@ -4,3 +4,27 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10
 });
+
+// nav toggle
+
+function navToggle() {
+    document.getElementById('navBurger').classList.toggle('_active');
+}
+
+// footer toggle
+
+function infoToggle() {
+    var footerInfo = document.getElementById("footerInfo");
+    footerInfo.classList.toggle("db");
+}
+
+
+function contactsToggle() {
+    var footerContacts = document.getElementById("footerContacts");
+    footerContacts.classList.toggle("db");
+}
+
+function hourToggle() {
+    var footerHours = document.getElementById("footerHours");
+    footerHours.classList.toggle("db");
+}
